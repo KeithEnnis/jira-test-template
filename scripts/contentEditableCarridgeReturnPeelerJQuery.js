cfbc8b04841fa400626f8465a8a32868339a3fd6
KeithEnnis <keith.ennis@solarwinds.com>
2018-11-20 16:39:15 +0000

    Update contentEditableCarridgeReturnPeelerJQuery.js
    annotations

--- scripts/contentEditableCarridgeReturnPeelerJQuery.js
+++ scripts/contentEditableCarridgeReturnPeelerJQuery.js
@@ -1,8 +1,8 @@
-$('[contenteditable]').on('paste', function(eventPassedIn) {
+$('[contenteditable]').on('paste', function(eventPassedIn) {// when pasting into a contenteditable
      	var $self = $(this);
-      setTimeout(function() {
-       	$self.html($self.text());
+      setTimeout(function() {//immediately
+       	$self.html($self.text());//convert to HTML thus removing line breaks
        }, 0);
-    }).on('keypress', function(eventPassedIn){
-    	 return eventPassedIn.which != 13;
+    }).on('keypress', function(eventPassedIn){//when a key is pressed
+    	 return eventPassedIn.which != 13;// dont recognise the return key being pressed
     });//keeps /n out of the text fields in the table so it doesn't screw up the export formatting.
